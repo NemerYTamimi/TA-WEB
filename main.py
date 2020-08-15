ce407a1265f87637a85d6ad5a0b1d75fc1f29c91
Nemer Y Tamimi <nemertamimi1999@icloud.com>
2020-08-15 22:14:06 +0300

    1st sharable commit

--- main.py
+++ main.py
@@ -1,29 +1,203 @@
-import os
-
+from flask import Flask, render_template, request, flash, url_for
 from pytube import YouTube
+from shutil import copyfile
+import groupdocs_conversion_cloud
+from app import *
+import os
+from werkzeug.utils import secure_filename
+
+# Get your app_sid and app_key at https://dashboard.groupdocs.cloud (free registration is required).
+UPLOAD_FOLDER = './static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-# #ask for the link from user
-# link = input("Enter the link of YouTube video you want to download:  ")
-# yt = YouTube(link)
-#
-# #Showing details
-# print("Title: ",yt.title)
-# print("Number of views: ",yt.views)
-# print("Length of video: ",yt.length)
-# print("Rating of video: ",yt.rating)
-# #Getting the highest resolution possible
-# ys = yt.streams.get_highest_resolution()
-#
-# #Starting download
-# print("Downloading...")
-# ys.download()
-# print("Download completed!!")
+app_sid = "2b861515-ac5b-4264-84aa-0df3f95c0596"
+app_key = "d35894bb7edb207cae4ff9da4b9931c2"
 
-def ydawnloader(url):
-    object = YouTube(url)
-    return object
+# Create instance of the API
+convert_api = groupdocs_conversion_cloud.ConvertApi.from_keys(app_sid, app_key)
+file_api = groupdocs_conversion_cloud.FileApi.from_keys(app_sid, app_key)
 
 
-yt = YouTube("https://www.youtube.com/watch?v=cMYGLYM135k")
-print(yt.streams.filter(only_audio=True,file_extension='mp4').all())
+ALLOWED_EXTENSIONS = {'pdf', 'doc', 'docx'}
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def get_url():
+    try:
+        global yt, url
+        url = request.form['url']
+        print(url)
+        yt = YouTube(url)
+        str = "<select name=resolutions id='selectres'>"
+        for i in yt.streams.filter(file_extension='mp4', progressive='true'):
+            str += f"<option onclick='download();' value='{i.itag}'>Video(mp4){i.resolution}</option>"
+        for i in yt.streams.filter(only_audio=True, file_extension='mp4'):
+            str += f"<option onclick='download();' value='{i.itag}'>Audio(mp3)</option>"
+        str += "</select><button id='requestbtn' onclick='download();'>Request Download Link</button>"
+        return str
+    except:
+        return '<a href="/">Something error <strong>click here </strong>to refresh the page</a> '
+
+
+def downlaod():
+    try:
+        res = request.form['res']
+        print(res)
+        ys = yt.streams.get_by_itag(res)
+        x = ys.title
+        y = ''
+        for character in x:
+            if character.isalnum():
+                y += character
+        x = ''
+        y += str(res)
+        ys.download("./static/downloads/", filename=y)
+        resolution = ys.resolution
+        if ys.itag == 140:
+            type = 'Audio'
+            resolution = '128kbps'
+        else:
+            type = 'Video'
+        a = f"<a id='download_link' href=./static/downloads/{y}.mp4 target='_blank' download >Download {resolution} {type}</a>"
+        return a
+    except:
+        return '<a href="/">some thing error click here to refresh the page</a>'
+
+
+def doc2pdf(file, filename, output_file, output_name):
+    request_upload = groupdocs_conversion_cloud.UploadFileRequest(file, filename)
+    response_upload = file_api.upload_file(request_upload)
+    # Create necessary API instances
+    apiInstance = groupdocs_conversion_cloud.ConvertApi.from_keys(app_sid, app_key)
+
+    # Prepare convert settings
+    settings = groupdocs_conversion_cloud.ConvertSettings()
+    settings.file_path = file
+    settings.format = "pdf"
+
+    load_options = groupdocs_conversion_cloud.DocxLoadOptions()
+    load_options.password = "password"
+    convert_options = groupdocs_conversion_cloud.PdfConvertOptions()
+    convert_options.center_window = True
+    convert_options.compress_images = False
+    convert_options.display_doc_title = True
+    convert_options.dpi = 1024.0
+    convert_options.fit_window = False
+    convert_options.from_page = 1
+    convert_options.grayscale = False
+    convert_options.image_quality = 100
+    convert_options.linearize = False
+    convert_options.margin_top = 5
+    convert_options.margin_left = 5
+    convert_options.unembed_fonts = True
+    convert_options.remove_unused_streams = True
+    convert_options.remove_unused_objects = True
+    convert_options.remove_pdfa_compliance = False
+
+    settings.load_options = load_options
+    settings.convert_options = convert_options
+    settings.output_path = "converted"
+    result = apiInstance.convert_document(groupdocs_conversion_cloud.ConvertDocumentRequest(settings))
+    request_download = groupdocs_conversion_cloud.DownloadFileRequest(f"converted/{output_name}")
+    response_download = file_api.download_file(request_download)
+    copyfile(response_download, f"./{output_file}")
+    # Convert
+    return render_template("index.html", script=f'<script>x=null</script>')
+
+
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return 'No selected file'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            output_name = filename.rsplit('.', 1)[0] + '.doc'
+            output_file = f"./static/downloads/{output_name}"
+            uploaded_file = f"./static/uploads/{filename}"
+            print(uploaded_file)
+            pdf2doc(filename, uploaded_file, output_file, output_name)
+            return f"<a id='download_link' href={output_file} target='_blank' download >Download </a>"
+
+
+def pdf2doc(file, filename, output_file, output_name):
+    try:
+
+        # upload soruce file to storage
+
+        remote_name = file
+        strformat = 'doc'
+        request_upload = groupdocs_conversion_cloud.UploadFileRequest(remote_name, filename)
+        response_upload = file_api.upload_file(request_upload)
+        # Convert PDF to Word document
+        settings = groupdocs_conversion_cloud.ConvertSettings()
+        settings.file_path = remote_name
+        settings.format = strformat
+        settings.output_path = output_name
+
+        loadOptions = groupdocs_conversion_cloud.PdfLoadOptions()
+        # Convert PDF to Word document
+        settings = groupdocs_conversion_cloud.ConvertSettings()
+        settings.file_path = remote_name
+        settings.format = strformat
+        settings.output_path = output_name
+
+        loadOptions = groupdocs_conversion_cloud.PdfLoadOptions()
+        loadOptions.hide_pdf_annotations = True
+        loadOptions.remove_embedded_files = False
+        loadOptions.flatten_all_fields = True
+
+        settings.load_options = loadOptions
+        #
+        # convertOptions = groupdocs_conversion_cloud.DocxConvertOptions()
+        # # convertOptions.from_page = 1
+        # # convertOptions.pages_count = 10
+
+        settings.convert_options = groupdocs_conversion_cloud.DocxConvertOptions()
+
+        request = groupdocs_conversion_cloud.ConvertDocumentRequest(settings)
+        response = convert_api.convert_document(request)
+
+        print("Document converted successfully: " + str(response))
+        # upload source file to storage
+
+        request_download = groupdocs_conversion_cloud.DownloadFileRequest(output_name)
+        response_download = file_api.download_file(request_download)
+        copyfile(response_download, f"./{output_file}")
+
+    except groupdocs_conversion_cloud.ApiException as e:
+        print("Exception when calling get_supported_conversion_types: {0}".format(e.message))
+    return render_template("index.html", script=f'<script>x=null</script>')
+
+
+def upload_file2():
+    if request.method == 'POST':
+        print(request.form)
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return 'No selected file'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            output_name = filename.rsplit('.', 1)[0] + '.pdf'
+            output_file = f"./static/downloads/{output_name}"
+            uploaded_file = f"./static/uploads/{filename}"
+            doc2pdf(filename, uploaded_file, output_file, output_name)
+            return f"<a id='download_link' href={output_file} target='_blank' download >Download </a>"
+        return "error"
--- static/js/script.js
+++ static/js/script.js
@@ -40,15 +40,23 @@
 
 })(jQuery);
 
-window.onload=function () {
+window.onload = function () {
 }
+
 function loading() {
-    document.getElementById('img').style.display='block';
+    document.getElementById('img').style.display = 'block';
 }
+
+function submit() {
+
+}
+
+submit = getInfo
+
 function getInfo() {
     loading();
-    document.getElementById('img').style.display='block';
-    link=document.getElementById('url').value
+    document.getElementById('img').style.display = 'block';
+    link = document.getElementById('url').value
     var ajax = new XMLHttpRequest();
     var method = "POST";
     var url = "/index";
@@ -58,15 +66,66 @@ function getInfo() {
     ajax.send('url='.concat(link));
     ajax.onreadystatechange = function () {
         if ((this.readyState == 4) && (this.status = 200)) {
-            let result=document.getElementById("result")
+            let result = document.getElementById("result")
             result.innerHTML = this.responseText;
         }
-        document.getElementById('img').style.display='none';
+        document.getElementById('img').style.display = 'none';
 
     }
 }
+
+//var files =document.getElementById('url').files;
+//document.getElementById('url').onchange = function(event) {
+//    files = document.getElementById('url').files;
+//}
+//function pdf2word() {
+//    loading();
+//    document.getElementById('img').style.display='block';
+//    var ajax = new XMLHttpRequest();
+//    var method = "POST";
+//    var url = "/uploader";
+//    files=document.getElementById('url').files
+//    var file=files[0]
+//    var asynchronous = true;
+//    ajax.open(method, url, asynchronous);
+//    ajax.setRequestHeader('Content-Type', 'multipart/form-data');
+//    var formData = new FormData();
+//    formData.append('file', file);
+//    ajax.send(formData)
+//    ajax.onreadystatechange = function () {
+//        if ((this.readyState == 4) && (this.status = 200)) {
+//            let result=document.getElementById("result")
+//            result.innerHTML = this.responseText;
+//        }
+//        document.getElementById('img').style.display='none';
+//
+//    }
+//}
+//function word2pdf() {
+//    loading();
+//    document.getElementById('img').style.display='block';
+//    var ajax = new XMLHttpRequest();
+//    var method = "POST";
+//    var url = "/uploader1";
+//    files=document.getElementById('url').files
+//    var file=files[0]
+//    var asynchronous = true;
+//    ajax.open(method, url, asynchronous);
+//    ajax.setRequestHeader('Content-Type', 'multipart/form-data');
+//    var formData = new FormData();
+//    formData.append('file', file);
+//    ajax.send(formData)
+//    ajax.onreadystatechange = function () {
+//        if ((this.readyState == 4) && (this.status = 200)) {
+//            let result=document.getElementById("result")
+//            result.innerHTML = this.responseText;
+//        }
+//        document.getElementById('img').style.display='none';
+//
+//    }
+//}
 function download() {
-    document.getElementById('img').style.display='block';
+    document.getElementById('img').style.display = 'block';
     var ajax = new XMLHttpRequest();
     var e = document.getElementById("selectres");
     var res = e.options[e.selectedIndex].value;
@@ -78,12 +137,43 @@ function download() {
     ajax.send('res='.concat(res));
     ajax.onreadystatechange = function () {
         if ((this.readyState == 4) && (this.status = 200)) {
-            a=document.createElement('div')
-            a.className='linkclass'
+            a = document.createElement('div')
+            a.className = 'linkclass'
             document.getElementById('result').append(a)
-            a.innerHTML=this.responseText
+            a.innerHTML = this.responseText
         }
-        document.getElementById('img').style.display='none';
+        document.getElementById('img').style.display = 'none';
 
     }
+}
+
+function y_function() {
+    document.getElementById('url').name = 'url';
+    document.getElementById('url').type = 'text';
+    document.getElementById('submit').innerText = 'GetInfo';
+    submit = getInfo;
+    document.getElementById('main_label').innerHTML = '<span>T</span><span>A</span><span>-</span><span>W</span><span>E</span><span>B</span><br><span>Y</span><span>o</span><span>u</span><span>t</span><span>u</span><span>b</span><span>e</span><span> </span><span>D</span><span>o</span><span>w</span><span>n</span><span>l</span><span>o</span><span>a</span><span>d</span><span>e</span><span>r</span>';
+    let formHTML = '<input id="url" type="text" name="url" placeholder="Enter Link here"><button id="submit" onclick="submit();">Get Info</button><div class="section vertical-center-row"><img src="/static/30.svg" id="img" style="display: none;margin-left: 50%;"></div><div id="result"></div>'
+    document.getElementById('form').innerHTML = formHTML
+    document.getElementById('op1').style = "background-color: royalblue;"
+}
+
+function pdf2word_function() {
+    submit = pdf2word;
+    document.getElementById('main_label').innerHTML = '<span>T</span><span>A</span><span>-</span><span>W</span><span>E</span><span>B</span><br><span>P</span><span>D</span><span>F</span><span>-</span><span>T</span><span>o</span><span>-</span><span>W</span><span>o</span><span>r</span><span>d</span><span>';
+    let formHTML = '<form method="post" action="/uploader" enctype="multipart/form-data"> <input id="url" type="file" name="file" placeholder="Choose file"><button id="submit" onclick="submit();">Convert</button></form><div class="section vertical-center-row"><img src="/static/30.svg" id="img" style="display: none;margin-left: 50%;"></div><div id="result"></div>'
+    document.getElementById('form').innerHTML = formHTML;
+    document.getElementById('url').style = 'padding-top: 10px;padding-bottom: 30px;';
+    document.getElementById('op2').style = "background-color: royalblue;"
+
+}
+
+function word2pdf_function() {
+    submit = pdf2word;
+    document.getElementById('main_label').innerHTML = '<span>T</span><span>A</span><span>-</span><span>W</span><span>E</span><span>B</span><br><span>W</span><span>o</span><span>r</span><span>d</span><span>-</span><span>T</span><span>o</span><span>-</span><span>P</span><span>D</span><span>F</span><span>';
+    let formHTML = '<form method="post" action="/uploader1" enctype="multipart/form-data"> <input id="url" type="file" name="file" placeholder="Choose file"><button id="submit" onclick="submit();">Convert</button></form><div class="section vertical-center-row"><img src="/static/30.svg" id="img" style="display: none;margin-left: 50%;"></div><div id="result"></div>'
+    document.getElementById('form').innerHTML = formHTML;
+    document.getElementById('url').style = 'padding-top: 10px;padding-bottom: 30px;';
+    document.getElementById('op3').style = "background-color: royalblue;"
+
 }
